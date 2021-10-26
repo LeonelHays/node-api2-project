751ff6d5d90396991420b15f8518fc3e5d3d6e4e
Leo Hays <starryhornet@gmail.com>
2021-10-26 15:45:28 -0500

    done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -57,6 +57,74 @@ router.post('/', (req, res) => {
     }
 })
 
-router.delete('')
+router.delete('/:id', async (req, res) => {
+    try{
+        const post = await Posts.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }else{
+            await Posts.remove(req.params.id)
+            res.json(post)
+        }
+    }catch(error){
+        res.status(500).json({
+            message: "The post could not be removed"
+        })
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const {title, contents} = req.body
+    if(!title || !contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }else {
+        Posts.findById(req.params.id)
+            .then(stuff => {
+                if(!stuff){
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                }else{
+                    return Posts.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if(data){
+                    return Posts.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                res.json(post)
+            })
+            .catch(error => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    error: error.message
+                })
+            })
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    try{
+        const post = await Posts.findById(req.params.id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else{
+            const comments = await Posts.findPostComments(req.params.id)
+            res.json(comments)
+        }
+    } catch(err){
+        res.status(500).json({
+            message: "The comments information could not be retrieved"
+        })
+    }
+})
 
 module.exports = router;
